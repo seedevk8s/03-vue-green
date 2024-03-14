40262588fbe590e1f469c323ca83335f8c2d9ca5
intelliwebservfullstack <intelliwebservfullstack@gmail.com>
2024-03-14 11:20:09 +0900

    좋아요 click 이벤트 추가

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <h3 class="bg-yellow">{{movie.title}}</h3>
     <p>개봉: {{movie.year}}</p>
     <p>장르: {{movie.category}}</p>
+    <button @:click="increaseLike(i)">좋아요</button> <span>{{movie.like}}</span>
   </div>
 </template>
 
@@ -17,21 +18,30 @@
             title: "노량",
             year: 2023,
             category: "액션, 드라마",
-            textRed: "color: red",            
+            textRed: "color: red",  
+            like: 0,          
           },
           {
             title: "아쿠아맨과 로스트 킹덤",
             year: 2023,
-            category: "액션, 판타지, 어드벤처",        
+            category: "액션, 판타지, 어드벤처",
+            like: 0,          
           },
           {
             title: "3일의 휴가",
             year: 2023,
-            category: "판타지, 드라마",         
+            category: "판타지, 드라마",    
+            like: 0,       
           },
         ]
       }
+    },
+    methods: {
+      increaseLike(i) {
+        this.data[i].like += 1
+      }
     }
+
   }
 </script>
 
